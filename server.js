992eca6a3774d4d4785c1529e20e5cfdf43ede9a
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-05-19 17:53:11 +0200

    added mongoose

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const app = require("./app");
 
-app.listen(3000, () => {});
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Tinkkid:mDqbXyXNzfsEVcPo@cluster0.lz1m311.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.env(1);
+  });
