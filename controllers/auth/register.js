381840b93f5d5464a2d6cf16c1da3d29876e5a35
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-05-30 08:48:53 +0200

    Created hashPassword with bcrypt

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,15 @@
 const User = require("../../models/user");
+const { HttpError } = require("../../helpers/HttpError");
+const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  const newUser = await User.create(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, hashPassword });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
   res.status(201).json({
     status: "success",
     code: 201,
